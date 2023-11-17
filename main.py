5151c4e06f3c7640742eac3aca52b32b0c05494c
“TonyLu12138” <tony.lu@feixitek.com>
2023-11-17 07:56:46 +0000

    主函数类

---
+++ main.py
@@ -0,0 +1,64 @@
+#! /usr/bin/env python3
+
+import argparse
+from log_record import Logger
+from replacement_installation import Re_In_
+
+# 更换内核
+def replace_kernel(re_in):
+    re_in.change_kernel()
+
+# 检查内核版本
+def check_kernel_version(re_in):
+    re_in.check_kernel_version()
+
+# 安装 VersaSDS DEB
+def install_versds_deb(re_in):
+    re_in.install_versasds_deb()
+
+# 卸载 VersaSDS DEB
+def ubinstall_versds_deb(re_in):
+    re_in.uninstall_versasds_deb()
+
+# 显示 DRBD/LINSTOR 版本
+def display_drbd_linstor_version(re_in):
+    re_in.get_versions()
+
+def display_version():
+    print("version: v1.0.0")
+
+def main():
+    parser = argparse.ArgumentParser(description='None')
+    parser.add_argument('-r', '--replace', action='store_true',
+                        help='Replacement of the kernel')
+    parser.add_argument('-c', '--check', action='store_true',
+                        help='Checking the kernel version')
+    parser.add_argument('-iV', '--install', action='store_true',
+                        help='Install VersaSDS DEB')
+    parser.add_argument('-uV', '--uninstall', action='store_true',
+                        help='Uninstall VersaSDS DEB')
+    parser.add_argument('-d', '--display', action='store_true',
+                        help='')
+    parser.add_argument('-v', '--version', action='store_true',
+                        help='Show version information')
+    args = parser.parse_args()
+
+    logger = Logger("log")
+    re_in = Re_In_(logger)
+    
+    if args.replace:
+        replace_kernel(re_in)
+    elif args.check:
+        check_kernel_version(re_in)
+    elif args.install:
+        install_versds_deb(re_in)
+    elif args.uninstall:
+        ubinstall_versds_deb(re_in)
+    elif args.display:
+        display_drbd_linstor_version(re_in)
+    elif args.version:
+        display_version()
+    else:
+        print(f"请输入对应参数")
+if __name__ == '__main__':
+    main()
