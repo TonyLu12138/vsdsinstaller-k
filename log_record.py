bede24c48c8a580290412996378be257c43a035d
TonyLu12138 <138006045+TonyLu12138@users.noreply.github.com>
2023-12-07 23:07:58 +0800

    调整了日志产生逻辑以及命名方式

--- log_record.py
+++ log_record.py
@@ -1,6 +1,6 @@
 #! /usr/bin/env python3
 import logging
-from logging.handlers import RotatingFileHandler
+from logging.handlers import TimedRotatingFileHandler
 import os
 import sys
 import datetime
@@ -12,17 +12,16 @@ log_directory = os.path.join(current_directory, "logs")  # 日志文件夹路径
 if not os.path.exists(log_directory):
     os.makedirs(log_directory)  # 如果文件夹不存在，创建它
 
-
 class Logger:
     def __init__(self, task_name, debug_enabled=False):
         self.logger = logging.getLogger(f"Debug_{task_name}")
         self.logger.setLevel(logging.DEBUG)
-        self.name = f"log-{datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.log"
+        self.name = f"{task_name}_{datetime.datetime.now().strftime('%Y-%m-%d')}.log"
         self.debug_log_file = os.path.join(log_directory, self.name)
         self.debug_enabled = debug_enabled  # Set initial debug state
 
-        file_handler = RotatingFileHandler(
-            self.debug_log_file, maxBytes=1000 * 1024 * 1024, backupCount=5)
+        file_handler = TimedRotatingFileHandler(
+            self.debug_log_file, when='midnight', interval=1, backupCount=5)
         file_handler.setLevel(logging.DEBUG)
         formatter = logging.Formatter(
             '%(asctime)s - %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
@@ -31,4 +30,3 @@ class Logger:
 
     def log(self, message):
         self.logger.debug(message)
-
